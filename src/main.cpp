140b2eb2a99e1d95c52be8b31f8c1de2942209ec
RandyPrimaPutra <103095236+randyprimaputra@users.noreply.github.com>
2022-12-05 23:39:06 +0800

    Hello World

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
  
 // For compilers that support precompilation, includes "wx/wx.h".
 #include <wx/wxprec.h>
- 
+#include <MyFrame.h>
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
 #endif
@@ -13,21 +13,7 @@ public:
     virtual bool OnInit();
 };
  
-class MyFrame : public wxFrame
-{
-public:
-    MyFrame();
- 
-private:
-    void OnHello(wxCommandEvent& event);
-    void OnExit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
-};
- 
-enum
-{
-    ID_Hello = 1
-};
+
  
 wxIMPLEMENT_APP(MyApp);
  
@@ -38,44 +24,3 @@ bool MyApp::OnInit()
     return true;
 }
  
-MyFrame::MyFrame()
-    : wxFrame(nullptr, wxID_ANY, "Hello World")
-{
-    wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-                     "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
- 
-    wxMenu *menuHelp = new wxMenu;
-    menuHelp->Append(wxID_ABOUT);
- 
-    wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
-    menuBar->Append(menuHelp, "&Help");
- 
-    SetMenuBar( menuBar );
- 
-    CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
- 
-    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
-    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
-    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
-}
- 
-void MyFrame::OnExit(wxCommandEvent& event)
-{
-    Close(true);
-}
- 
-void MyFrame::OnAbout(wxCommandEvent& event)
-{
-    wxMessageBox("This is a wxWidgets Hello World example",
-                 "About Hello World", wxOK | wxICON_INFORMATION);
-}
- 
-void MyFrame::OnHello(wxCommandEvent& event)
-{
-    wxLogMessage("Hello world from wxWidgets!");
-}
